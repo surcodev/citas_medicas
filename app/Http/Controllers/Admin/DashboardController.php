44ae402f61ae5c26d5f3a1cd2ed8904cf5c1a093
richarq-fisi <temporal98x@gmail.com>
2025-10-20 00:32:23 -0500

    domingo

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\AppointmentEnum;
 use App\Http\Controllers\Controller;
+use App\Models\Appointment;
+use App\Models\Doctor;
+use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
@@ -11,6 +16,19 @@ class DashboardController extends Controller
     public function index()
     {
         Gate::authorize('access_dasboard');
-        return view('admin.dashboard');
+
+        $data=[];
+
+        $data['total_patients'] = Patient::count();
+        $data['total_doctors'] = Doctor::count();
+        $data['total_appointments'] = Appointment::whereDate('created_at', now())
+            ->where('status', AppointmentEnum::SCHEDULED)
+            ->count();
+        $data['recent_users'] = User::latest()
+            ->take(5)
+            ->get();
+
+
+        return view('admin.dashboard',compact('data'));
     }
 }
